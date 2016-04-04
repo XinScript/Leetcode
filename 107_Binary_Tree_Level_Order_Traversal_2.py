371c936393165c57aa5c676356da47d783873fc1
XinScript <hobsonchan1992@gmail.com>
2016-04-04 17:31:59 +0800

    quesiont 102 and 107

---
+++ 107_Binary_Tree_Level_Order_Traversal_2.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if root is None:
+            return []
+        else:
+            quene = [root]
+            result = []
+            currentNum = 1
+            nextNum = 0
+            while quene:
+                current = []
+                while currentNum > 0:
+                    node = quene.pop(0)
+                    current.append(node.val)
+                    currentNum -= 1
+                    if node.left:
+                        quene.append(node.left)
+                        nextNum+= 1
+                    if node.right:
+                        quene.append(node.right)
+                        nextNum += 1
+                result.append(current)
+                currentNum=nextNum
+                nextNum = 0
+        result.reverse()
+        return result
