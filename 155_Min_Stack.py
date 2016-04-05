244bc8ada5cbd961abc6f296bb8bfe96f068374a
XinScript <hobsonchan1992@gmail.com>
2016-04-05 23:15:01 +0800

    question 155

---
+++ 155_Min_Stack.py
@@ -0,0 +1,49 @@
+class MinStack(object):
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.min = []
+        self.size = 0
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: nothing
+        """
+        self.stack.append(x)
+        if self.size == 0 or x<=self.min[-1]:
+            self.min.append(x)
+        self.size+=1
+    def pop(self):
+        """
+        :rtype: nothing
+        """
+        if self.size:
+            top = self.stack.pop()
+            self.size-=1
+            if top == self.min[-1]:
+                self.min.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        if self.size:
+            return self.stack[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        if self.size:
+            return self.min[-1]
+#
+m = MinStack()
+m.push(-2)
+m.push(0)
+m.push(-1)
+print(m.getMin())
+print(m.getMin())
+print(m.getMin())
+print(m.top())
