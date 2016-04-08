60a2a6f0f8e62d603ea140cc72c2afceb4f91542
XinScript <hobsonchan1992@gmail.com>
2016-04-08 19:55:43 +0800

    improve the time complexity to nlogn

--- 300_Longest_Increasing_Subsequence.py
+++ 300_Longest_Increasing_Subsequence.py
@@ -25,3 +25,51 @@ class Solution(object):
                 if i > maxCount:
                     maxCount = i
             return maxCount
+
+    def lengthOfLIS2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def binarySearch(left, right, nums, n):
+            while left < right:
+                mid = (left + right) // 2
+                if nums[mid] < n:
+                    left = mid + 1
+                elif nums[mid] > n:
+                    right = mid - 1
+                else:
+                    left = mid
+                    break
+            if nums[left] < n:
+                return left
+            else:
+                return left-1
+
+        l = len(nums)
+        if l == 0:
+            return 0
+        elif l == 1:
+            return 1
+        else:
+            result = [1 for i in range(0, len(nums))]
+            result[0] = 1
+            temp = [None,nums[0]]
+            for i in range(1, l):
+                j = binarySearch(1,len(temp)-1,temp,nums[i])
+                if j == len(temp)-1:
+                    result[i] = result[i-1]+1
+                    temp.append(nums[i])
+
+                else:
+                    result[i] = result[i-1]
+                    if j == 0 and nums[i]<temp[1]:
+                        temp[1] = nums[i]
+                    if temp[j+1]>nums[i]:
+                        temp[j+1] = nums[i]
+
+            maxCount = 1
+            for i in result:
+                if i > maxCount:
+                    maxCount = i
+            return maxCount
