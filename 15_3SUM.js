754d939f0b45c64900e29674381fbc551ca3557e
XinScript <hobsonchan1992@gmail.com>
2017-02-06 00:19:03 +0000

    15

---
+++ 15_3SUM.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+	nums.sort();
+	var result = [];
+	for (var i = 0; i < nums.length - 2; i++) {
+		if (i === 0 || (i > 0 && nums[i] != nums[i - 1])) {
+			var low = i + 1;
+			var high = nums.length - 1;
+			var sum = 0 - nums[i];
+			while (low < high) {
+				if (nums[low] + nums[high] === sum) {
+					result.push([nums[i], nums[low], nums[high]]);
+					while (low < high && nums[low] == nums[low + 1]) low++;
+					while (low < high && nums[high] == nums[high - 1]) high--;
+					low++;
+					high--;
+				} else if (nums[low] + nums[high] > sum) high--;
+				else low++;
+			}
+		}
+	}
+	return result;
+}
