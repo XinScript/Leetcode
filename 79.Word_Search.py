364f96db9f9578c1d4a4495b7dc1fbdebf091387
XinScript <hobsonchan1992@gmail.com>
2017-11-03 23:08:28 +0000

    79

---
+++ 79.Word_Search.py
@@ -0,0 +1,24 @@
+class Solution:
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        def search(i,j,k):
+        	if k == len(word):
+        		return True
+        	if 0 <= i <= len(board) - 1 and 0<= j <= len(board[0]) - 1:
+	        	if board[i][j] == word[k]:
+	        		board[i][j] = ord(board[i][j])
+		        	rs = search(i-1,j,k+1) or search(i+1,j,k+1) or search(i,j-1,k+1) or search(i,j+1,k+1)
+		        	board[i][j] = chr(board[i][j])
+		        	return rs
+	        return False
+
+       	for i,row in enumerate(board):
+       		for j,ele in enumerate(row):
+       				if search(i,j,0):
+       					return True
+       	return False
+
